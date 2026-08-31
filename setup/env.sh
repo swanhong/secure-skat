75479b996babbe46b246b9e0306e83d883d685b6
Seungwan Hong <swanhong92@gmail.com>
2026-08-31 15:02:33 -0400

    add ld library path

--- setup/env.sh
+++ setup/env.sh
@@ -1,3 +1,4 @@
 export R_LIBS_USER="${R_LIBS_USER:-$HOME/R/library}"
+export LD_LIBRARY_PATH="$(Rscript -e 'cat(R.home("lib"))')${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}"
 export PLINK2="${PLINK2:-$HOME/plink2}"
 export PATH="$HOME:$PATH"
