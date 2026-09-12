0bd05523ab7471b8b7bf6edbb913e45de7e9b7b8
Seungwan Hong <swanhong92@gmail.com>
2026-09-12 17:52:31 -0400

    add time in sh

--- scripts/run_aou_workflow.sh
+++ scripts/run_aou_workflow.sh
@@ -6,37 +6,38 @@ source scripts/setup/env.sh
 
 config_path="${CONFIG_PATH:-config/aou}"
 reference_engine="${REFERENCE_ENGINE:-python}"
+TIMEFORMAT='  wall time: %R seconds'
 
 echo "[0/7] Prepare AoU chromosome inputs"
-python3 python/datasets/aou/prepare_aou.py \
+time python3 python/datasets/aou/prepare_aou.py \
   --config "${config_path}"
 
 echo "[1/7] Prepare ancestry-specific secure inputs"
-go run -mod=vendor secure-rvas.go prepare \
+time go run -mod=vendor secure-rvas.go prepare \
   --config "${config_path}" \
 
 echo "[2/7] Generate shared PRG keys"
-go run -mod=vendor secure-rvas.go keygen \
+time go run -mod=vendor secure-rvas.go keygen \
   --config "${config_path}"
 
 echo "[3/7] Run secure Burden/SKAT"
-go run -mod=vendor secure-rvas.go run \
+time go run -mod=vendor secure-rvas.go run \
   --config "${config_path}"
 
 echo "[4/7] Run ancestry-specific ${reference_engine} reference"
-python3 python/analysis/run_reference.py \
+time python3 python/analysis/run_reference.py \
   --config "${config_path}" \
   --engine "${reference_engine}"
 
 echo "[5/7] Compare secure and reference results"
-python3 python/analysis/compare_secure_to_reference.py \
+time python3 python/analysis/compare_secure_to_reference.py \
   --config "${config_path}"
 
 echo "[6/7] Generate scatter and Manhattan plots"
-python3 python/analysis/plot_secure_vs_reference.py \
+time python3 python/analysis/plot_secure_vs_reference.py \
   --config "${config_path}"
 
 echo "[7/7] Summarize metrics"
-./scripts/summarize_metrics.sh "${config_path}"
+time ./scripts/summarize_metrics.sh "${config_path}"
 
 echo "Secure RVAS AoU workflow completed"
